bc1dbde7b5bfc9b1123b7ccdeaa8286fcbcdc644
openevocracy <carlo.michaelis@gmail.com>
2015-08-13 08:57:16 +0000

    proposal pad can only be edited in proposal stage

--- routes/proposals.js
+++ routes/proposals.js
@@ -11,6 +11,7 @@ exports.query = function(req, res) {
     var tid = ObjectId(req.params.id);
     var uid = ObjectId(req.signedCookies.uid);
     
+    var topic = {};
     var requestValid = _.after(2, function() {
         // get proposal or create proposal if it does not exist
         // from http://stackoverflow.com/questions/16358857/mongodb-atomic-findorcreate-findone-insert-if-nonexistent-but-do-not-update
@@ -26,16 +27,21 @@ exports.query = function(req, res) {
                     proposal.body = body;
                     res.json(proposal);
                 });
+                
+                // pad can only be edited in proposal stage
+                if(topic.stage != C.STAGE_PROPOSAL)
+                    delete proposal.pid;
         });
     });
     var requestInvalid = function() {
         res.status(400).send('Bad Request');
     }
     
-    // check if topic is in proposal stage
+    // check if topic is at least in proposal stage
     console.log('tid ' + JSON.stringify(tid));
-    db.collection('topics').findOne({ '_id': tid }, function(err, topic) {
-        if(topic.stage >= C.STAGE_PROPOSAL)
+    db.collection('topics').findOne({ '_id': tid }, function(err, topic_) {
+        topic = topic_;
+        if(topic_.stage >= C.STAGE_PROPOSAL)
             requestValid();
         else
             requestInvalid();
