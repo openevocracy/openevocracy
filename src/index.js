a6bfca876c0f8902af34bc656f6c7544058332b8
openevocracy <carlo.michaelis@gmail.com>
2017-08-17 18:47:05 +0000

    theming and Angular translate

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@
 var angular = require("angular");
 
 // Define app
-var module = angular.module('evocracy', []);
+var module = angular.module('evocracy', ['pascalprecht.translate']);
+
+module.config(function ($translateProvider) {
+  $translateProvider.translations('en', {
+    TOPICS: 'Topics'
+  });
+  $translateProvider.translations('de', {
+    TOPICS: 'Themen'
+  });
+  $translateProvider.preferredLanguage('en');
+});
 
 // Fade out loading spinner
 $('#loading').fadeOut();
 
 // Define topic list controller
-module.controller('TopicListController', function($http) {
+module.controller('TopicListController', function($scope, $translate) {
     // Request topic collection via get ajax
-    $http({
+    /*$http({
         method : 'GET', url : 'json/topics'
     }).then(function success(res) {
         this.topics = res.data;
     }.bind(this), function error(res) {
         console.error(res);
-    });
+    });*/
+    
+    $scope.changeLanguage = function (key) {
+        $translate.use(key);
+    };
 });
 
