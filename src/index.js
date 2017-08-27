1b25d0976cb77d843a452635821d29f9a5293187
The OpenEvocracy developers <develop@openevocracy.org>
2017-08-27 12:17:25 -0400

    Angular routing, footer and internationalization

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ var angular = require("angular");
 var bootstrap = require("bootstrap"); // TODO load it in webpack.config.js -> plugins
 var angularTranslate = require("angular-translate");
 var angularTranslateFiles = require("angular-translate-loader-partial");
+var angularRoute = require("angular-route");
+var version = require('../package.json').version
 
 // Define app
-var module = angular.module('evocracy', [angularTranslate]);
+var module = angular.module('evocracy', [angularTranslate, angularRoute]);
 
 module.config(function($translateProvider, $translatePartialLoaderProvider) {
 	// Register a loader for the static files
@@ -19,11 +21,35 @@ module.config(function($translateProvider, $translatePartialLoaderProvider) {
 	$translateProvider.preferredLanguage('de');
 });
 
+module.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			name: 'topics',
+			templateUrl: "topics.html",
+			controller: "TopicListController"
+		})
+		.when('/topics', {
+			name: 'topics',
+			templateUrl: "topics.html",
+			controller: "TopicListController"
+		})
+		.when('/timeline', {
+			name: 'timeline',
+			templateUrl: "timeline.html",
+			controller: "TimelineController"
+		});
+}]);
+
 // Define header controller
 module.controller('HeaderController', function($scope, $translate) {
 	$scope.setLanguage = function(lang) {
 		$translate.use(lang);
 	};
+});
+
+// Define header controller
+module.controller('FooterController', function($scope) {
+	$scope.version = version;
 });
 
 // Define topic list controller
@@ -36,6 +62,13 @@ module.controller('TopicListController', function($scope, $http) {
 	}.bind(this), function error(res) {
 		console.error(res);
 	});
+});
+
+// Define timeline controller
+module.controller('TimelineController', function($scope, $translate) {
+	$scope.setLanguage = function(lang) {
+		$translate.use(lang);
+	};
 });
 
 
