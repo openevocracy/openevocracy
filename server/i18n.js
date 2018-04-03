574a8411a569d497b498e5b5b536fce442983f07
Patrick Charrier <patrick.charrier@gmail.com>
2018-04-03 20:18:21 +0000

    move i18n to shared folder

--- server/i18n.js
+++ server/i18n.js
@@ -1,10 +1,9 @@
 var i18next = require('i18next');
 var Promise = require('bluebird');
-//var requirejs = require('requirejs');
 
 // TODO replace "en" by language of specific user from database
-var en; // = requirejs('public/js/app/nls/en/mail');
-var de; // = requirejs('public/js/app/nls/de/mail');
+var en = require('./i18n/de.json'); // = requirejs('public/js/app/nls/en/mail');
+var de = require('./i18n/en.json'); // = requirejs('public/js/app/nls/de/mail');
 
 exports.initAsync = Promise.promisify(i18next.init).bind(i18next)({
     lng: 'en', // TODO de
