69027619df6a4aed2b51d816ea9954812b8bd65a
sagacitysite <carlo.michaelis@gmail.com>
2019-02-26 21:04:51 +0000

    minor changes

--- server/routes/forums.js
+++ server/routes/forums.js
@@ -11,7 +11,7 @@ exports.queryForum = function(req, res) {
 	var forumId = ObjectId(req.params.id);
 	//var userId = ObjectId(req.user._id);
 	
-	// Get group and topic
+	// Get group, group pad and topic
 	var group_promise = db.collection('groups').findOneAsync({'forumId': forumId}, {'topicId': true});
 	var pad_promise = group_promise.then(function(group) {
 		return db.collection('pads_group').findOneAsync({'groupId': group._id}, {'_id': true});
