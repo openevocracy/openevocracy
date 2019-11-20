fc2279ac82263b2b90a7c52007176f2d995e60b3
Patrick Charrier <patrick.charrier@gmail.com>
2019-11-20 21:58:54 +0100

    big refactor topics

--- server/routes/topics.js
+++ server/routes/topics.js
@@ -22,8 +22,8 @@ function calculateDeadline(nextStage, prevDeadline, levelDuration) {
     if(_.isUndefined(levelDuration))
         levelDuration = cfg.DURATION_LEVEL;
     
-    var nextDeadline = _.isUndefined(prevDeadline) ? Date.now() : prevDeadline;
-    //var nextDeadline = Date.now();
+    let nextDeadline = _.isUndefined(prevDeadline) ? Date.now() : prevDeadline;
+    //let nextDeadline = Date.now();
     switch (nextStage) {
         case C.STAGE_SELECTION: // get selection stage deadline
             nextDeadline += cfg.DURATION_SELECTION;
@@ -47,10 +47,10 @@ exports.calculateDeadline = calculateDeadline;
 function manageConsensusStageAsync(topic, levelDuration) {
     // levelDuration is an optional parameter
     // it will get a default value in calculateDeadline if not set (for testing purposes)
-    var prevDeadline = topic.nextDeadline;
+    let prevDeadline = topic.nextDeadline;
     
     return groups.manage.remixGroupsAsync(topic).then(function(result) {
-        var update_set_promise;
+        let update_set_promise;
         switch(result.nextStage) {
         case C.STAGE_CONSENSUS:
             // updates below are only required while we are in consensus stage
@@ -60,7 +60,7 @@ function manageConsensusStageAsync(topic, levelDuration) {
             };
             break;
         case C.STAGE_PASSED:
-				var topicId = topic._id;
+				let topicId = topic._id;
 				
 				// get the pad id of final document
 				// e.g. the proposal pad id of the group in the last level
@@ -74,7 +74,7 @@ function manageConsensusStageAsync(topic, levelDuration) {
             
             // Get pad PDF and save it in file system
             finalDocumentHtml_promise.then(pads.getPadPDFAsync).then(function(data) {
-                var filename = path.join(appRoot.path, 'files/documents', topicId+'.pdf');
+                let filename = path.join(appRoot.path, 'files/documents', topicId+'.pdf');
                 return fs.writeFileAsync(filename,data);
             });
             
@@ -153,45 +153,43 @@ exports.manageAndListTopicsAsync = manageAndListTopicsAsync;
  * @param {object} topic - a topic from database
  * @return {object} topic - adjusted topic
  */
-function manageTopicStateAsync(topic) {
+async function manageTopicStateAsync(topic) {
     
 	// Exit this function if stage transition is not due yet
 	if(Date.now() < topic.nextDeadline)
 		return Promise.resolve(topic);
 			
 	// Move to next stage
-	var prevDeadline = topic.nextDeadline;
+	let prevDeadline = topic.nextDeadline;
 	switch (topic.stage) {
 		case C.STAGE_SELECTION: // we are currently in selection stage
-			return isAccepted(topic).then(function(isAccepted) {
-				var stageStartedEntryName;
-				if(isAccepted) {
-					// Topic does meet the minimum requirements for the next stage, move to next stage
-					topic.stage = C.STAGE_PROPOSAL;
-					topic.nextDeadline = calculateDeadline(C.STAGE_PROPOSAL,prevDeadline); // get deadline for proposal stage
-					stageStartedEntryName = 'stageProposalStarted';
-					topic[stageStartedEntryName] = Date.now();
-				} else {
-					// Topic has been rejected, move to rejection stage
-					topic.stage = C.STAGE_REJECTED;
-					topic.rejectedReason = 'REJECTED_NOT_ENOUGH_VOTES';
-					stageStartedEntryName = 'stageRejectedStarted';
-					topic[stageStartedEntryName] = Date.now();
-				}
-				var updateTopicStatePromise = updateTopicStateAsync(topic,stageStartedEntryName);
-				var updatePadExpirationPromise = db.collection('pads_topic_description').updateAsync(
-					{ '_id': topic.pid }, { $set: { 'expiration': Date.now() }}
-				);
-				
-				return Promise.join(updateTopicStatePromise, updatePadExpirationPromise).
-				return(topic);
-			});
+			let stageStartedEntryName;
+			if(await isAccepted(topic)) {
+				// Topic does meet the minimum requirements for the next stage, move to next stage
+				topic.stage = C.STAGE_PROPOSAL;
+				topic.nextDeadline = calculateDeadline(C.STAGE_PROPOSAL,prevDeadline); // get deadline for proposal stage
+				stageStartedEntryName = 'stageProposalStarted';
+				topic[stageStartedEntryName] = Date.now();
+			} else {
+				// Topic has been rejected, move to rejection stage
+				topic.stage = C.STAGE_REJECTED;
+				topic.rejectedReason = 'REJECTED_NOT_ENOUGH_VOTES';
+				stageStartedEntryName = 'stageRejectedStarted';
+				topic[stageStartedEntryName] = Date.now();
+			}
+			const updateTopicStatePromise = updateTopicStateAsync(topic,stageStartedEntryName);
+			const updatePadExpirationPromise = db.collection('pads_topic_description').updateAsync(
+				{ '_id': topic.pid }, { $set: { 'expiration': Date.now() }}
+			);
+			
+			await Promise.join(updateTopicStatePromise, updatePadExpirationPromise);
+			return topic;
 		case C.STAGE_PROPOSAL: // we are currently in proposal stage
 			// Set the next deadline here, so we can use it in createGroupsAsync.
 			topic.nextDeadline = calculateDeadline(C.STAGE_CONSENSUS,prevDeadline);
 			
 			return groups.manage.createGroupsAsync(topic).then(function(groups) {
-				var stageStartedEntryName;
+				let stageStartedEntryName;
 				
 				if(_.size(groups) >= cfg.MIN_GROUPS_PER_TOPIC) {
 					topic.stage = C.STAGE_CONSENSUS;
@@ -208,6 +206,28 @@ function manageTopicStateAsync(topic) {
 			}).spread(function(topic, stageStartedEntryName) {
 				return updateTopicStateAsync(topic, stageStartedEntryName).return(topic);
 			});
+
+			// ------------------------------------
+			/*// Set the next deadline here, so we can use it in createGroupsAsync.
+			topic.nextDeadline = calculateDeadline(C.STAGE_CONSENSUS,prevDeadline);
+			
+			const groups = await groups.manage.createGroupsAsync(topic);
+				
+			let stageStartedEntryName;
+			if(_.size(groups) >= cfg.MIN_GROUPS_PER_TOPIC) {
+				topic.stage = C.STAGE_CONSENSUS;
+				stageStartedEntryName = 'stageConsensusStarted';
+			} else {
+				topic.stage = C.STAGE_REJECTED;
+				topic.nextDeadline = cfg.DURATION_NONE;
+				topic.rejectedReason = 'REJECTED_NOT_ENOUGH_VALID_USER_PROPOSALS';
+				stageStartedEntryName = 'stageRejectedStarted';
+			}
+			
+			topic[stageStartedEntryName] = Date.now();
+			
+			await updateTopicStateAsync(topic, stageStartedEntryName);
+			return topic;*/
 		case C.STAGE_CONSENSUS: // we are currently in consensus stage
 			return manageConsensusStageAsync(topic);
 	}
@@ -215,151 +235,101 @@ function manageTopicStateAsync(topic) {
 	return Promise.resolve(topic);
 }
 
-function appendTopicInfoAsync(topic, userId, with_details) {
-	var topicId = topic._id;
+async function appendTopicInfoAsync(topic, userId, with_details) {
+	const topicId = topic._id;
 	
 	// Get number of participants and votes in this topic
-	var topic_votes_promise = db.collection('topic_votes')
+	const topic_votes = await db.collection('topic_votes')
 		.find({'topicId': topicId}, {'userId': true}).toArrayAsync();
-	var pads_proposal_count_promise = db.collection('pads_proposal').countAsync({'topicId': topicId});
+	const pads_proposal_count = await db.collection('pads_proposal').countAsync({'topicId': topicId});
     
 	// TODO http://stackoverflow.com/questions/5681851/mongodb-combine-data-from-multiple-collections-into-one-how
 	
 	// Get groups and sort by level
-	var groups_promise = db.collection('groups')
+	const groups = await db.collection('groups')
 		.find({ 'topicId': topicId }, { 'level': true })
 		.sort({ 'level': -1 }).toArrayAsync();
 	
 	// Number of participants per level
-	var participants_per_levels_promise = groups_promise.then(function(groups) {
-		var memberCountsPerGroups_promise =
-		db.collection('group_relations').aggregateAsync( [
-			{ $match: { 'groupId': { $in: _.pluck(groups, '_id') } } },
-			{ $group: { '_id': '$groupId', member_count: { $sum : 1 } } } ] );
-		
-		return Promise.join(groups, memberCountsPerGroups_promise);
-	}).spread(function (groups, member_counts_per_groups) {
-		var member_counts_per_groups_sorted_by_levels =
-		_.groupBy(member_counts_per_groups, function(member_count) {
-			var group = utils.findWhereObjectId(groups, {'_id': member_count._id});
-			return group.level;
-		});
-        
-		var member_counts_per_levels =
-		_.mapObject(member_counts_per_groups_sorted_by_levels,
-		function(member_counts_per_groups, level) {
-			var member_counts_per_level = _.reduce(member_counts_per_groups,
-				function(memo, member_counts_per_group) {
-					return memo + member_counts_per_group.member_count;
-				}, 0);
-				return member_counts_per_level;
-		});
-        
-		return member_counts_per_levels;
-	});
+	const memberCountsPerGroups = await
+	db.collection('group_relations').aggregateAsync( [
+		{ $match: { 'groupId': { $in: _.pluck(groups, '_id') } } },
+		{ $group: { '_id': '$groupId', member_count: { $sum : 1 } } } ] );
 	
-	// Number of groups per level
-	var groups_per_levels_promise = groups_promise.then(function(groups) {
-		if(_.isEmpty(groups))
-			return null;
-		
-		// count groups by level
-		return _.countBy(groups, function(group) {return group.level;});
+	const member_counts_per_groups_sorted_by_levels =
+	_.groupBy(memberCountsPerGroups, function(member_count) {
+		const group = utils.findWhereObjectId(groups, {'_id': member_count._id});
+		return group.level;
 	});
-	
-	// Combine #participants and #groups
-	var levels_promise =
-		Promise.join(participants_per_levels_promise, groups_per_levels_promise)
-			.spread(function(participants_per_levels, groups_per_levels) {
-				var levels_object = _.mapObject(participants_per_levels,function(participants_per_level, level) {
-					return { 'participants': participants_per_level, 'groups': groups_per_levels[level] };
-			});
-		return _.toArray(levels_object);
-	});
-	
-	// Detailed data (not used in topic list, only in details)
-	var pad_description_promise = null;
-	var user_proposal_promise = null;
-	var user_group_promise = null;
     
+	const participants_per_levels =
+	_.mapObject(member_counts_per_groups_sorted_by_levels,
+	function(member_counts_per_groups, level) {
+		return _.reduce(member_counts_per_groups,
+			(memo, member_counts_per_group) => memo + member_counts_per_group.member_count, 0);
+	});
+
+	// Number of groups per level
+	const groups_per_levels = _.countBy(groups, group => group.level);
+
+	// Combine #participants and #groups
+	const levels_object = _.mapObject(participants_per_levels,
+		(participants_per_level, level) => ({ 'participants': participants_per_level, 'groups': groups_per_levels[level] }));
+	const levels = _.toArray(levels_object);
+
+	// Detailed data (not used in topic list, only in details)
+	let user_group;
 	if(with_details) {
 		// Get topic description
-		pad_description_promise = db.collection('pads_topic_description')
+		pad_description = await db.collection('pads_topic_description')
 			.findOneAsync({'topicId': topicId}, { 'docId': true, 'ownerId': true })
-			.then(function(pad) {
-				return addHtmlToPad('topic_description', pad);
-		});
-		
+			.then(pad => addHtmlToPad('topic_description', pad));
+
 		// Get proposal of user
-		user_proposal_promise = db.collection('pads_proposal')
+		user_proposal = await db.collection('pads_proposal')
 			.findOneAsync({ 'topicId': topicId, 'ownerId': userId }, { 'docId': true, 'ownerId': true })
-			.then(function(pad) {
-				return _.isNull(pad) ? null : addHtmlToPad('proposal', pad);
-		});
-		
+			.then(pad => _.isNull(pad) ? null : addHtmlToPad('proposal', pad));
+
 		// Find the group that the current user is part of (in last level)
-		user_group_promise = groups_promise.then(function(groups) {
-			if (_.isEmpty(groups))
-				return null;
+		// Get all groups in highest level
+		const highest_level = _.max(groups, group => group.level).level;
+		const highest_level_groups = _.filter(groups, group => group.level == highest_level);
+		
+		// Find that highest_level_groups, where user is part of and return that group
+		const group_member = await db.collection('group_relations')
+			.findOneAsync({'groupId': { $in: _.pluck(highest_level_groups, '_id') }, 'userId': userId});
+		
+		// Get group pad information
+		if (group_member) {
+			const group = utils.findWhereObjectId(highest_level_groups, {'_id': group_member.groupId});
+			const pad = await db.collection('pads_group').findOneAsync({'groupId': group._id}, {'docId': true});
 			
-			return groups_promise.then(function(groups) {
-				// Get all groups in highest level
-				var highest_level = _.max(groups, function(group) {return group.level;}).level;
-				var highest_level_groups = _.filter(groups, function(group) { return group.level == highest_level; });
-				
-				// Find that highest_level_groups, where user is part of and return that group
-				var group_promise = db.collection('group_relations')
-					.findOneAsync({'groupId': { $in: _.pluck(highest_level_groups, '_id') }, 'userId': userId})
-					.then(function(member) {
-						if (_.isNull(member))
-							return null;
-						return utils.findWhereObjectId(highest_level_groups, {'_id': member.groupId});
-				});
-				
-				// Get group pad information
-				var pad_promise = group_promise.then(function(group) {
-					if (_.isNull(group))
-						return null;
-					return db.collection('pads_group').findOneAsync({'groupId': group._id}, {'docId': true});
-				});
-				
-				return Promise.join(group_promise, pad_promise);
-			}).spread(function(group, pad) {
-				if (_.isNull(group)) {
-					return null;
-				} else {
-					// Add pad id, doc id and html to group
-					var groupAndPad = _.extend(group, { 'padId': pad._id, 'docId': pad.docId });
-					return addHtmlToPad('group', groupAndPad);
-				}
-			});
-		});
+			// Add pad id, doc id and html to group
+			const groupAndPad = _.extend(group, { 'padId': pad._id, 'docId': pad.docId });
+			user_group = addHtmlToPad('group', groupAndPad);
+		}
 	}
 	
 	// Basic topic details
-	var topic_without_details_promise = Promise.props(_.extend(topic,{
-		'numVotes': topic_votes_promise.then(_.size),
-		'numProposals': pads_proposal_count_promise,
-		'voted': topic_votes_promise.then(function(topic_votes) {
-			return utils.checkArrayEntryExists(topic_votes, {'userId': userId});
-		}),
-		'levels': levels_promise
+	const topic_without_details = await Promise.props(_.extend(topic,{
+		'numVotes': _.size(topic_votes),
+		'numProposals': pads_proposal_count,
+		'voted': utils.checkArrayEntryExists(topic_votes, {'userId': userId}),
+		'levels': levels
 	}));
     
 	// TODO Check if this is really a good way doing it
 	
 	if(with_details) {
 		// Extended topic information for topic view
-		return topic_without_details_promise.then(function(topic_without_details) {
-			return Promise.props(_.extend(topic_without_details, {
-				'group': user_group_promise,
-				'proposal': user_proposal_promise,
-				'description': pad_description_promise
-			}));
-		});
+		return _.extend(topic_without_details, {
+				'group': user_group,
+				'proposal': user_proposal,
+				'description': pad_description
+			});
 	} else {
 		// Basic topic information for topic list
-		return topic_without_details_promise;
+		return topic_without_details;
 	}
 }
 
@@ -443,7 +413,7 @@ exports.query = async function(req, res) {
 };
 
 exports.create = async function(req, res) {
-   // Topic name is the only necessary request variable
+   // Topic name is the only necessary request letiable
    const data = req.body;
    const userId = ObjectId(req.user._id);
    const topic = {name: data.name};
@@ -470,9 +440,9 @@ exports.create = async function(req, res) {
    await db.collection('topics').insertAsync(topic);
    
    // Create description
-   /*var dpid = ObjectId(); // Create random pad id
-   var description = { 'pid': dpid, 'tid': topic._id };
-   var createTopicDescriptionPromise = db.collection('topic_descriptions').insertAsync(description);*/
+   /*let dpid = ObjectId(); // Create random pad id
+   let description = { 'pid': dpid, 'tid': topic._id };
+   let createTopicDescriptionPromise = db.collection('topic_descriptions').insertAsync(description);*/
    
    // Create pad
    const pad = { 'topicId': topic._id, 'ownerId': userId, 'expiration': topic.nextDeadline };
